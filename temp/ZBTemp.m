ccab546a369dbe69ccec9681bc2dd047ac0e6921
Arslan Asim <arslanasim@Arslan-Asim.local>
2015-07-01 20:21:54 +0500

    temp files

---
+++ temp/ZBTemp.m
@@ -0,0 +1,40 @@
+//
+//  ZBTemp.m
+//  ZapBam
+//
+//  Created by Arslan Asim on 30/06/2015.
+//  Copyright (c) 2015 Arslan Asim. All rights reserved.
+//
+
+#import "ZBTemp.h"
+
+@implementation ZBTemp
++(BOOL)propertyIsOptional:(NSString*)propertyName
+{
+    return YES;
+}
+@end
+@implementation ImportantModel
++(BOOL)propertyIsOptional:(NSString*)propertyName
+{
+    return YES;
+}
+@end
+@implementation NutrientsDetailsModel
++(BOOL)propertyIsOptional:(NSString*)propertyName
+{
+    return YES;
+}
+@end
+@implementation NutrientsModel
++(BOOL)propertyIsOptional:(NSString*)propertyName
+{
+    return YES;
+}
+@end
+@implementation PortionsModel
++(BOOL)propertyIsOptional:(NSString*)propertyName
+{
+    return YES;
+}
+@end
